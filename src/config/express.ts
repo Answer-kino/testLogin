219a2f90eed93a109477c89a18684aa43f685f19
Answer-kino <kino@ianswer.co.kr>
2022-07-12 18:33:48 +0900

    :book:Config: Express Configuration

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,10 +1,39 @@
 import express from "express";
-import dotenv from "dotenv";
+import path from "path";
+import morgan from "morgan";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
+import { Config } from "src/config/config";
+import { logger, stream } from "src/config/logger";
+import indexRotuer from "src/route/index";
 
-import indexRotuer from "../route/index";
-
-dotenv.config();
 const app = express();
+
+/**
+ * express JSON & URLENCODED
+ */
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
+
+/**
+ * Logger [ morgan ]
+ */
+app.use(morgan("combined", { stream }));
+
+/**
+ * Route
+ */
 app.use("/", indexRotuer);
+
+/**
+ * Swagger
+ */
+if (Config.server.mode !== "production") {
+  logger.info("Swagger On : /api-docs");
+  const swaggerSpec = YAML.load(path.join(__dirname, "../../dist/swagger.yaml"));
+  const swaggerOptions = { docExpansion: "none" };
+  const swaggerUiOptions = { explorer: true };
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions, swaggerOptions));
+}
 
 export default app;
